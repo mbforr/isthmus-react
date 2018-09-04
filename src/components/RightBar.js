e2cab9e51fcb16a97fa049eb95efef24bf12e980
mbforr <mforrest@carto.com>
2018-09-04 09:55:38 -0400

    fix RightBar.js

--- src/components/RightBar.js
+++ src/components/RightBar.js
@@ -6,9 +6,6 @@ import carto, { filter, source, style, layer  } from '@carto/carto.js';
 import Category from './widgets/Category'
 import airship from '@carto/airship-style'
 
-// import { Category, Slider } from './widgets'
-//import Slider from './widgets/Slider'
-
 class RightBar extends Component {
 
   constructor(props) {
@@ -16,70 +13,9 @@ class RightBar extends Component {
       this.state = {
         ...props
       }
-
   }
 
- //  componentDidMount() {
- //    this._addDataview();
- //  }
- //
- //
- //
- //  _addDataview() {
- //    this.dataView = new carto.dataview.Category(this.props.layers.railaccidents.source, 'railroad', {
- //      limit: 10,
- //      operation: carto.operation.SUM,
- //      operationColumn: 'total_damage',
- //    });
- //
- //    this.dataView.on('dataChanged', ({ categories }) => this.setState({ categories }));
- //    this.props.client.addDataview(this.dataView);
- // }
- //
- //
- //   componentDidUpdate(prevProps) {
- //     const bboxFilter = new carto.filter.BoundingBoxLeaflet(this.props.map)
- //     // this.dataView.addFilter(this.props.boundingbox);
- //     // this.dataView.on('dataChanged', this.onDataChanged);
- //
- //     if(prevProps !== this.props) {
- //       this.dataView.addFilter(this.props.boundingbox);
- //     }
- //
- //   }
- //
- //  _createFilter() {
- //    const filter = new carto.filter.Category('railroad', { in: this.state.selection });
- //    this.props.layers.railaccidents.source.addFilter(filter);
- //    this.setState({  filter });
- //  }
- //
- //  _updateFilter() {
- //    this.filter.setFilters({ in: this.state.selection });
- //  }
- //
- //  onSelectedChanged = ({ detail }) => {
- //    let { filter } = this.state;
- //
- //    if (filter && !detail.length) {
- //      this.props.layers.railaccidents.source.removeFilter(filter);
- //      filter = null;
- //    }
- //
- //    this.setState({ selection: detail, filter });
- //  }
- //
- //  onApplySelection = () => {
- //    const { filter, selection } = this.state;
- //
- //    selection.length > 0 && !filter
- //      ? this._createFilter()
- //      : this._updateFilter();
- //  }
-
   render() {
-    const { categories, filter, selection } = this.state;
-
 
     return (
       <div>
