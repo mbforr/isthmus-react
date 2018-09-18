fcc61ba85d7188d79ab7ac1e0332385a25afad7b
mbforr <mforrest@carto.com>
2018-09-18 16:55:01 -0400

    cleanup class conflicts

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 
 const InfoWindow = ({ railroad, cause, weather, accident_type, image, city }) => (
   <as-infowindow src={image}>
-    <h1 class="as-title">{city}</h1>
+    <h1 className="as-title">{city}</h1>
     <p>
-    <span class="as-badge">{accident_type}</span>
+    <span className="as-badge">{accident_type}</span>
     </p>
     <br />
-    <p class="as-caption">{cause}</p>
+    <p className="as-caption">{cause}</p>
     <br />
     <h4 className="as-subheader">Company</h4>
-    <p class="as-body">{railroad}</p>
+    <p className="as-body">{railroad}</p>
     <br />
     <h4 className="as-subheader">Weather</h4>
-    <p class="as-body">{weather}</p>
+    <p className="as-body">{weather}</p>
 </as-infowindow>
 );
 
